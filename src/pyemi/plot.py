a35e5c8081dfab42adb6fc41afe79af76b841ac7
Emi <76208994+emilianomfortes@users.noreply.github.com>
2023-07-27 16:07:44 +0200

    Update plot.py

--- src/pyemi/plot.py
+++ src/pyemi/plot.py
@@ -8,7 +8,12 @@ def plot_dimensions():
   dims["in_to_pt"] = 72.27
   dims["in_to_cm"] = 2.54
   dims["cm_to_pt"] = 28.45
+  dims["pt_to_in"] = 1.0/72.27
 
+  #
+  # Page sizes
+  #
+  
   dims["A4"] = {}
   dims["A4"]["in"] = {}
   dims["A4"]["pt"] = {}
@@ -19,5 +24,14 @@ def plot_dimensions():
   dims["A4"]["pt"]["height"] = 842
   dims["A4"]["cm"]["width"] = 21
   dims["A4"]["cm"]["height"] = 29.7
+
+  #
+  # Journal LaTeX parameters
+  #
+  dims["Journal"] = {}
+  dims["Journal"]["elsevier-twocolumn-A4"] = {}
+  dims["Journal"]["elsevier-twocolumn-A4"]["pt"] = {}
+  dims["Journal"]["elsevier-twocolumn-A4"]["pt"]["margin"] = 34.51605
+  dims["Journal"]["elsevier-twocolumn-A4"]["pt"]["top"] = 52.74657
 
   return dims
