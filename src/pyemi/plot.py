bb5a1fee2a1a1df41df82040aa927e541886ef39
Emi <76208994+emilianomfortes@users.noreply.github.com>
2023-08-14 14:47:06 +0200

    Update plot.py

--- src/pyemi/plot.py
+++ src/pyemi/plot.py
@@ -37,8 +37,8 @@ def plot_dimensions():
   dims["Journal"]["elsevier-twocolumn-A4"]["pt"]["top"] = 52.74657
 
   dims["Journal"]["elsevier-twocolumn-A4"]["Figure"] = {}
-  dims["Journal"]["elsevier-twocolumn-A4"]["Figure"]["Single column - width"]["pt"] = 255
-  dims["Journal"]["elsevier-twocolumn-A4"]["Figure"]["1.5 column - width"]["pt"] = 297
-  dims["Journal"]["elsevier-twocolumn-A4"]["Figure"]["Double column - width"]["pt"] = 539
+  dims["Journal"]["elsevier-twocolumn-A4"]["Figure"]["Single column_width_pt"] = 255
+  dims["Journal"]["elsevier-twocolumn-A4"]["Figure"]["1.5 column_width_pt"] = 297
+  dims["Journal"]["elsevier-twocolumn-A4"]["Figure"]["Double column_width_pt"] = 539
   
   return dims
