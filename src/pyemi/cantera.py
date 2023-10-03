4b7510c74ee0e5308bc8e1e564e903e8253b9eeb
Emi <76208994+emilianomfortes@users.noreply.github.com>
2023-10-03 10:57:34 +0200

    Update cantera.py

--- src/pyemi/cantera.py
+++ src/pyemi/cantera.py
@@ -172,7 +172,7 @@ def getZfromflux(df, gas, dict_bilger, beta_fuel, beta_oxi):
     return df["j_Z"]
 
 
-def getHeatRel(data, mech):
+def HeatReleaseRate(data, mech):
 
     if isinstance(data, pd.DataFrame):
         _data = data.copy()
